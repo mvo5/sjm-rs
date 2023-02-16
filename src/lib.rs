a3d2653f3b710c006933cbeadbe598b38483e9e1
Michael Vogt <mvo@ubuntu.com>
2023-02-16 21:49:13 +0100

    simplify code a bit, remove need for self.nonce

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,18 @@ use std::fmt;
 type HmacSha256 = Hmac<Sha256>;
 use base64::{engine::general_purpose, Engine as _};
 
-
-
 #[derive(Debug)]
 pub struct SignedJsonMessage {
     header: HashMap<String, String>,
     payload: HashMap<String, String>,
 
     key: String,
-    nonce: String,
 }
-
 
 impl SignedJsonMessage {
     pub fn new(key: &str, nonce: &str) -> SignedJsonMessage {
         SignedJsonMessage {
             key: key.to_string(),
-            // XXX: remove nonce
-            nonce: nonce.to_string(),
             // XXX: is there a better way to init defaults?
             header: HashMap::from([
                 ("ver".to_string(), "1".to_string()),
@@ -38,10 +32,14 @@ impl SignedJsonMessage {
     pub fn payload(&self) -> &HashMap<String, String> {
         return &self.payload;
     }
-    pub fn nonce(&self) -> &String {
-        return &self.nonce;
+    pub fn nonce(&self) -> Option<&String> {
+        return self.header.get("nonce");
     }
-    pub fn from_string(s: &str, key: &str, expected_nonce: &str) -> Result<SignedJsonMessage, String> {
+    pub fn from_string(
+        s: &str,
+        key: &str,
+        expected_nonce: &str,
+    ) -> Result<SignedJsonMessage, String> {
         let sp: Vec<&str> = s.rsplitn(2, ".").collect();
         if sp.len() != 2 {
             return Err("invalid input data {s}".to_string());
@@ -60,20 +58,22 @@ impl SignedJsonMessage {
         if sp.len() != 2 {
             return Err("invalid input header/payload {s}".to_string());
         }
-        let encoded_header = sp[0];
-        let encoded_payload = sp[1];
-        let header_bytes = general_purpose::STANDARD.decode(encoded_header).unwrap();
-        let payload_bytes = general_purpose::STANDARD.decode(encoded_payload).unwrap();
-        let header: HashMap<String, String> = serde_json::from_slice(header_bytes.as_slice()).expect("cannot read header json");
-        let payload: HashMap<String, String> = serde_json::from_slice(payload_bytes.as_slice()).expect("cannot read payload json");
+        let header_bytes = general_purpose::STANDARD.decode(sp[0]).unwrap();
+        let payload_bytes = general_purpose::STANDARD.decode(sp[1]).unwrap();
+        let header: HashMap<String, String> =
+            serde_json::from_slice(header_bytes.as_slice()).expect("cannot read header json");
+        let payload: HashMap<String, String> =
+            serde_json::from_slice(payload_bytes.as_slice()).expect("cannot read payload json");
         if expected_nonce != "" {
-            let nonce_item = header.get(&"nonce".to_string()).ok_or("cannot find nonce in header");
+            let nonce_item = header
+                .get(&"nonce".to_string())
+                .ok_or("cannot find nonce in header");
             let nonce = nonce_item.unwrap();
             if nonce != expected_nonce {
                 return Err("invalid nonce".to_string());
             }
         }
-        
+
         let mut msg = SignedJsonMessage::new(key, expected_nonce);
         msg.set_payload(payload);
         Ok(msg)
@@ -111,8 +111,7 @@ mod tests {
     fn create_signed_json_message() {
         let msg = SignedJsonMessage::new("key", "nonce");
         assert_eq!(msg.key, "key");
-        assert_eq!(msg.nonce, "nonce");
-        assert_eq!(msg.nonce(), "nonce");
+        assert_eq!(msg.nonce(), Some(&"nonce".to_string()));
     }
 
     #[test]
